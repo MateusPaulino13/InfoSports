ba69c2ddf33647689f93d72e1da93a63def5feb0
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2023-05-23 11:17:31 -0300

    Adicionando conexão

---
+++ src/pages/conecction.php
@@ -0,0 +1,10 @@
+<?php
+    // Create connection
+    $conn = mysqli_connect("localhost", "root", "");
+
+    // Check connection
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully";
+?>
