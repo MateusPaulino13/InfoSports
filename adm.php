155858853be9b351a96fe33ffaefe1da49d688e7
Mateus Paulino <mateusgpaulino@gmail.com>
2023-05-31 23:13:36 -0300

    Finalizando nivel de acesso

--- adm.php
+++ adm.php
@@ -19,6 +19,38 @@ if (isset($_SESSION['usuario']) && is_array($_SESSION['usuario'])) {
 </head>
 
 <body>
+    <?php if ($adm) : ?>
+        <table border="2">
+            <thead>
+                <tr>
+                    <td>Nome</td>
+                    <td>Email</td>
+                    <td>Senha</td>
+                    <td>ADM</td>
+                    <td>ID</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $query = $conexao->prepare("SELECT * FROM usuarios");
+                $query->execute();
+
+                $users = $query->fetchAll(PDO::FETCH_ASSOC);
+
+                for ($i = 0; $i < sizeof($users); $i++) :
+                    $usuarioAtual = $users[$i];
+                ?>
+                    <tr>
+                        <td><?php echo $usuarioAtual['Nome'] ?></td>
+                        <td><?php echo $usuarioAtual['Email'] ?></td>
+                        <td><?php echo $usuarioAtual['Senha'] ?></td>
+                        <td><?php echo $usuarioAtual['Adm'] ?></td>
+                        <td><?php echo $usuarioAtual['Id'] ?></td>
+                    </tr>
+                <?php endfor; ?>
+            </tbody>
+        </table>
+    <?php endif; ?>
     <button>
         <a href="logout.php">Sair</a>
     </button>
