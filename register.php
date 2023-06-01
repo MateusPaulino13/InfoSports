59b42bea31bf9695df0ac1039ed869cb32659fe9
Mateus Paulino <mateusgpaulino@gmail.com>
2023-05-31 22:12:46 -0300

    Adicionando registro e login

---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro - InfoSports</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <input type="text" name="email">
+        <input type="text" name="nome">
+        <input type="password" name="senha">
+        <input type="text" name="endereco">
+        <input type="submit" name="Entrar">
+    </form>
+</body>
+
+</html>
